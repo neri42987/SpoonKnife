a9cdd7d7573ea4bbfe7cf9b00e8a00ccb3872fa5
Kwok Wong <Kwok.Wong@tomtom.com>
2024-11-01 23:12:46 +0100

    update

--- create-gh-release.sh
+++ create-gh-release.sh
@@ -2,26 +2,31 @@
 
 TAG=$1
 TEMP_TAG=$1-stable
-RELEASE_NAME=alpha-stable/$TAG
+RELEASE_NAMME_PREFIX="alpha-stable/"
+RELEASE_NAME=$RELEASE_NAMME_PREFIX$TAG
 
+# Create a temporary tag to create a release
 tagcommit=$(git rev-list -n 1 $TAG)
 git tag $TEMP_TAG $tagcommit
 git push origin $TEMP_TAG
 
-# Create a release
+# Create the GH release
 gh release create $TEMP_TAG --title $RELEASE_NAME --notes "Release $RELEASE_NAME" --prerelease
 
 # Check if the release is created
 gh release list
 
-# Clean up
+# Clean up the temporary tag
 git tag -d $TEMP_TAG
 git push --delete origin $TEMP_TAG
-
 git fetch --prune --tags
 
 # Find last stable release
-gh release list | grep alpha-stable | head -n 1| awk '{print $1}'
+stableReleaseName=$(gh release list | grep alpha-stable | head -n 1| awk '{print $1}')
+stableReleaseVersion=${stableReleaseName/"$RELEASE_NAMME_PREFIX"/}
+echo "Last stable release: $stableReleaseName version=$stableReleaseVersion"
+stablecommit=$(git rev-list -n 1 $stableReleaseVersion)
+echo "alpha=$tagcommit stable=$stablecommit"
 
 
 
