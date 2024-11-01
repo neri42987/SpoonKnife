d7e7f418cc54ab6acfacd2be1e926c7c6a55a8c8
Kwok Wong <Kwok.Wong@tomtom.com>
2024-11-01 21:44:36 +0100

    update script

--- create-gh-release.sh
+++ create-gh-release.sh
@@ -1,6 +1,29 @@
 #!/usr/bin/env bash
 
-TAG=v0.0.3
+TAG=$1
+TEMP_TAG=$1-stable
+RELEASE_NAME=alpha-stable/$TAG
 
-git rev-list -n 1 $TAG
+tagcommit=$(git rev-list -n 1 $TAG)
+git tag $TEMP_TAG $tagcommit
+git push origin $TEMP_TAG
+
+# Create a release
+gh release create $TEMP_TAG --title $RELEASE_NAME --notes "Release $RELEASE_NAME" --prerelease
+
+# Check if the release is created
+gh release list
+
+# Clean up
+git tag -d $TEMP_TAG
+git push --delete origin $TEMP_TAG
+
+git fetch --prune --tags
+
+# Find last stable release
+gh release list | grep alpha-stable | head -n 2| awk '{print $1}'
+
+
+
+
 
