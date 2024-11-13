d727a01ba475224793649c8ae28da70e6a7b3a0e
unknown <aya.benayyad.quorris@gmail.com>
2024-11-13 18:15:16 +0100

    Ajout liste des Teams

--- script.js
+++ script.js
@@ -5,8 +5,30 @@ function showConsoleMessage() {
 console.log("ce message est affiché dans la console."); 
 }
 
+function addTeam(team) {
+    if (!teams.includes(team)) {
+        teams.push(team);
+        console.log(team + " has been added to the list");
+    } else {
+        console.log(team + " is already on the list");
+    }
+}
+//testttt 
+function showList() {
+    console.log("Finals liste :");
+    teams.forEach(function(team) {
+        console.log(team);
+    });
+}
+
+
 window.onload = function() {
 showAlert();
 showConsoleMessage();
+addTeam("T1");
+addTeam("GENG");
+addTeam("HLE");
+showList(); 
+
 }; 
 
